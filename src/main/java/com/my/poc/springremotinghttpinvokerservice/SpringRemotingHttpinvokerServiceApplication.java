3fab82e43a5eee8d8d44f2aebacf223dbcd8c92a
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-06-25 21:02:29 -0400

    Spring boot app skeleton setup

---
+++ src/main/java/com/my/poc/springremotinghttpinvokerservice/SpringRemotingHttpinvokerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.my.poc.springremotinghttpinvokerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRemotingHttpinvokerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRemotingHttpinvokerServiceApplication.class, args);
+	}
+
+}
